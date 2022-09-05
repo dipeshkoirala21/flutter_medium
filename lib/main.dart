99f506ba580c295029ae64e7533ad0804304ba19
Dipesh Koirala <dipeshkoirala21@gmail.com>
2022-09-05 15:54:58 +0545

    feat: stack

--- lib/main.dart
+++ lib/main.dart
@@ -34,18 +34,26 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: const Text('Flutter App'),
         ),
-        body: Column(
+        body: Stack(
           children: [
             Container(
-              height: 100,
-              width: 100,
+              height: 300,
+              width: 400,
               color: Colors.blue,
             ),
             Container(
-              height: 100,
-              width: 100,
+              height: 200,
+              width: 350,
               color: Colors.red,
-            )
+            ),
+            Positioned(
+                top: 50,
+                left: 100,
+                child: Container(
+                  height: 100,
+                  width: 150,
+                  color: Colors.black,
+                ))
           ],
         ));
   }
